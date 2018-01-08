8231a68577c9135f46c980ccdd4ca2b6aa49a1f5
Eduardo Pedroso <drdpedroso@gmail.com>
2018-01-08 14:26:22 -0200

    Fixing router

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/mitchellh/mapstructure"
 )
 
 type Message struct {
@@ -26,59 +23,35 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	router := &Router{}
+
+	router.Handle("channel add", addChannel)
+
+	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	socket, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		var inMessage Message
-		var outMessage Message
-		if err := socket.ReadJSON(&inMessage); err != nil {
-			fmt.Println(err)
-			break
-		}
-		fmt.Printf("%#v\n", inMessage)
-		switch inMessage.Name {
-		case "channel add":
-			err := addChannel(inMessage.Data)
-			outMessage = Message{"error", err}
-			if err := socket.WriteJSON(outMessage); err != nil {
-				fmt.Println(err)
-				break
-			}
-		case "channel subscribe":
-			go subscribeChannel(socket)
-		}
-	}
-}
+// // Receives an empty interface and returns a Channel and a Error
+// func addChannel(data interface{}) error {
+// 	var channel Channel
+// 	// We tell go for a more specific type (type assertion). channelMap its a map with string keys and empty interfaces as values
+// 	// channelMap := data.(map[string]interface{})
+// 	// channel.Name = channelMap["name"].(string)
 
-// Receives an empty interface and returns a Channel and a Error
-func addChannel(data interface{}) error {
-	var channel Channel
-	// We tell go for a more specific type (type assertion). channelMap its a map with string keys and empty interfaces as values
-	// channelMap := data.(map[string]interface{})
-	// channel.Name = channelMap["name"].(string)
+// 	err := mapstructure.Decode(data, &channel)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	channel.Id = "1"
+// 	fmt.Println("add new channel")
+// 	return nil
+// }
 
-	err := mapstructure.Decode(data, &channel)
-	if err != nil {
-		return err
-	}
-	channel.Id = "1"
-	fmt.Println("add new channel")
-	return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-	for {
-		time.Sleep(time.Second * 1)
-		message := Message{"channel add", Channel{"1", "Sofware Supp"}}
-		fmt.Println("sent new channel")
-		socket.WriteJSON(message)
-	}
-}
+// func subscribeChannel(socket *websocket.Conn) {
+// 	for {
+// 		time.Sleep(time.Second * 1)
+// 		message := Message{"channel add", Channel{"1", "Sofware Supp"}}
+// 		fmt.Println("sent new channel")
+// 		socket.WriteJSON(message)
+// 	}
+// }
